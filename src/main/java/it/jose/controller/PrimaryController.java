f043c693a773d4dfe3987321ec1a4ae85c530456
Benncio09 <lorebencini13@outlook.com>
2024-05-10 12:33:27 +0200

    menu-modificato

--- src/main/java/it/jose/controller/PrimaryController.java
+++ src/main/java/it/jose/controller/PrimaryController.java
@@ -4,12 +4,34 @@ import java.io.IOException;
 
 import it.jose.App;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 public class PrimaryController {
 
     @FXML
+    private Label messaggio;
+
+    @FXML
+    private RadioButton botAttivo;
+
+    @FXML
+    private ToggleGroup numeroGiocatori;
+
+    @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        RadioButton r = (RadioButton) numeroGiocatori.getSelectedToggle();
+        if (r == null) {
+            messaggio.setText("prima inserire il \nnumero di giocatori");
+        } else {
+            if (botAttivo.isSelected()) {
+                App.setRoot("secondary");
+
+            } else {
+                App.setRoot("tertiary");
+            }
+        }
 
     }
 }
